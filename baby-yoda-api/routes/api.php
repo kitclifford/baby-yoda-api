1db800614c54a9eb33d009a31874bd109aaf5d98
Kit Clifford-Page <kit.clifford.page@gmail.com>
2019-12-20 12:11:28 +0000

    Cleaned/edited some comments

--- baby-yoda-api/routes/api.php
+++ baby-yoda-api/routes/api.php
@@ -19,11 +19,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 $router->group(["prefix" => "baby_yoda"], function ($router) {
 
-    $router->post("", "BabyYodas@store");
+    $router->post("", "BabyYodas@store"); // store a yoda, needs name and colour 
+
     $router->get("", "BabyYodas@index"); // get all yodas
-    $router->get("{babyYoda}", "BabyYodas@show"); // to get a single yoda
-    $router->delete("{babyYoda}", "BabyYodas@destroy"); // kill a single yoda 
-    $router->post("/{babyYoda}/feed", "Feeds@store");  // to feed specific yoda 
-    $router->get("/{babyYoda}/feeds", "BabyYodas@show_feeds"); // get all feeds for a single yodas in decending order
-    $router->get("/find/{yodaName}", "BabyYodas@find_yoda");//find a baby yoda by name (name from app form)
+
+    $router->get("{babyYoda}", "BabyYodas@show"); // to get a single yoda (by ID)
+
+    $router->delete("{babyYoda}", "BabyYodas@destroy"); // delete a specific yoda (by ID)
+
+    $router->post("/{babyYoda}/feed", "Feeds@store");  // to feed specific yoda (by ID)
+
+    $router->get("/{babyYoda}/feeds", "BabyYodas@show_feeds"); // get all feeds for a single yoda (by ID) in decending order
+
+    $router->get("/find/{yodaName}", "BabyYodas@find_yoda");//find a baby yoda by name, get name, colour, dob and last feed in Unix time
 });
